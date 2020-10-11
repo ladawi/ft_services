5cb2c82ad12790623fd3b1d4cb51da61dcc3ae21
ladawi@student.42.fr <ladawi@student.42.fr>
2020-10-11 15:05:28 +0200

    -)

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 
 darkpurple='\e[0;35m'
 purple='\e[1;35m'
@@ -11,104 +11,120 @@ green='\e[1;32m'
 darkyellow='\e[0;33m'
 yellow='\e[1;33m'
 darkred='\033[1;31m'
-red='\e[38;5;160m'
+red='\e[0;31m'
 redbckgrnd='\033[48;5;160m'
 bold='\e[1m'
 underline='\e[4m'
-
 DRIVER=docker
+
+
+wait()
+{
+    pid=$!
+	while kill -0 $pid 2> /dev/null; do
+	    progress_bar $1
+	done
+}
+
+progress_bar() {
+  local duration=${1}
+
+
+    already_done() { for ((done=0; done<$elapsed; done++)); do printf "▇"; done }
+    remaining() { for ((remain=$elapsed; remain<$duration; remain++)); do printf " "; done }
+    percentage() { printf "| %s%%" $(( (($elapsed)*100)/($duration)*100/100 )); }
+    clean_line() { echo -en "\033[1K\r"; }
+
+  for (( elapsed=1; elapsed<=$duration; elapsed++ )); do
+      already_done; remaining; percentage
+      sleep 1
+      clean_line
+  done
+  clean_line
+}
 
 apply()
 {
-    case $1 in
-        "nginx")
-                echo "$yellow Nginx.ymal ... $neutre"
-                kubectl apply -f srcs/kustomization/wordpress.yaml > ./logs/kubectl_apply
-                echo "$green Nginx.yaml ✔ $neutre"
-                ;;
-        "wordpress")
-                echo "$yellow Wordpress.ymal ... $neutre"
-                kubectl apply -f srcs/kustomization/wordpress.yaml > ./logs/kubectl_apply
-                echo "$green Wordpress.yaml ✔ $neutre"
-                ;;
-        "mysql")
-                echo "$yellow Mysql.ymal ... $neutre"
-                kubectl apply -f srcs/kustomization/mysql.yaml > ./logs/kubectl_apply
-                echo "$green Mysql.yaml ✔ $neutre"
-                ;;
-        "php")
-                echo "$yellow Phpmyadmin.ymal ... $neutre"
-                kubectl apply -f srcs/kustomization/phpmyadmin.yaml > ./logs/kubectl_apply
-                echo "$green Phpmyadmin.ymal ✔ $neutre"
-                ;;
-        *)
-                echo "$yellow Kustomization file ... $neutre"
-                kubectl apply -k srcs/kustomization > ./logs/kubectl_apply
-                echo "$green Kustomization file ✔ $neutre"
-                ;;
-    esac
+    echo -e "$yellow Kustomization file ... $neutre"
+    kubectl apply -k srcs/kustomization &> ./logs/kubectl_apply &
+    wait 5
+    echo -e "$green Kustomization file ✔ $neutre"
 }
 
 start()
 {
-    sudo service docker start
-    echo "$green Docker ✔ $neutre"
-    minikube start --driver=$DRIVER > srcs/lauching_logs
-    echo "$green Minikube ✔ $neutre"
-    minikube addons enable metallb
-    minikube dashboard > logs/logs_dashboard &
+    echo -e "$yellow Docker ... $neutre"
+    sudo service docker start &
+    wait 2
+    echo -e "$green Docker ✔ $neutre"
+    minikube start --driver=$DRIVER &> logs/lauching_logs &
+    echo -e "$yellow Minikube ... $neutre"
+    wait 5
+    echo -e "$green Minikube ✔ $neutre"
+    minikube addons enable metallb &> logs/logs_metallb &
+    echo -e "$yellow metallb ... $neutre"
+    wait 5
+    echo -e "$green Metallb ✔ $neutre"
+    minikube dashboard &> logs/logs_dashboard &
+    echo -e "$yellow Dashboard ... $neutre"
+    echo -e "$green Dashboard Running ... $neutre"
 }
 
 stop()
 {
     sudo service docker stop
-    echo "$redbckgrnd Docker stopped $neutre"
+    echo -e "$redbckgrnd ${bold}Docker stopped 💀$neutre"
     sudo service nginx stop
-    echo "$redbckgrnd nginx stopped $neutre"
-    minikube stop
-    echo "$redbckgrnd Minikube stopped $neutre"
+    echo -e "$redbckgrnd ${bold}nginx stopped 💀$neutre"
+    minikube stop &> logs/logs_minikube_stop &
+    wait 4
+    echo -e "$redbckgrnd ${bold}Minikube stopped 💀$neutre"
 }
 
 prune()
 {
-    echo "$darkgreenback Cleaning ... $neutre"
+    echo -e "$darkgreenback Cleaning ... $neutre"
     docker system prune -f
     minikube ssh -- docker system prune -f
-    echo "$darkgreenback clean ✔ $neutre"
+    echo -e "$darkgreenback clean ✔ $neutre"
 }
 
 build()
 {
     if [ "$1" != "all" ]; then
         case $2 in
-                "eval")
-                        eval $(minikube -p minikube docker-env) > /dev/null
-                        echo "$bold Building images ${underline}in${neutre} ${bold}Minikube $neutre"
+                "out")
+                        echo -e "$bold Building images ${underline}outside${neutre} ${bold}Minikube $neutre"
                         ;;
                 *)
-                        echo "$bold Building images ${underline}outside${neutre} ${bold}Minikube $neutre"
+                        eval $(minikube -p minikube docker-env) > /dev/null
+                        echo -e "$bold Building images ${underline}in${neutre} ${bold}Minikube $neutre"
         esac
     fi
     case $1 in
             "nginx")
-                    echo "$yellow Building Nginx image ... $neutre"
-                    docker build -t nginx srcs/nginx/. > ./logs/logs_docker_nginx
-                    echo "$green Nginx image ✔ $neutre"
+                    echo -e "$yellow Building Nginx image ... $neutre"
+                    docker build -t nginx srcs/nginx/. > ./logs/logs_docker_nginx &
+                    wait 5
+                    echo -e "$green Nginx image ✔ $neutre"
                     ;;
             "wordpress")
-                    echo "$yellow Building Wordpress image ... $neutre"
-                    docker build -t wordpress srcs/wordpress/. > ./logs/logs_docker_wordpress
-                    echo "$green Wordpress image ✔ $neutre"
+                    echo -e "$yellow Building Wordpress image ... $neutre"
+                    docker build -t wordpress srcs/wordpress/. > ./logs/logs_docker_wordpress &
+                    wait 5
+                    echo -e "$green Wordpress image ✔ $neutre"
                     ;;
             "mysql")
-                    echo "$yellow Building Mysql image ... $neutre"
-                    docker build -t mysql srcs/mysql/. > ./logs/logs_docker_mysql
-                    echo "$green Mysql image ✔ $neutre"
+                    echo -e "$yellow Building Mysql image ... $neutre"
+                    docker build -t mysql srcs/mysql/. > ./logs/logs_docker_mysql &
+                    wait 5
+                    echo -e "$green Mysql image ✔ $neutre"
                     ;;
             "php")
-                    echo "$yellow Building Phpmyadmin image ... $neutre"
-                    docker build -t php srcs/phpmyadmin/. > ./logs/logs_docker_php
-                    echo "$green Phpmyadmin image ✔ $neutre"
+                    echo -e "$yellow Building Phpmyadmin image ... $neutre"
+                    docker build -t php srcs/phpmyadmin/. > ./logs/logs_docker_php &
+                    wait 5
+                    echo -e "$green Phpmyadmin image ✔ $neutre"
                     ;;
             *)
                     build nginx $2
@@ -124,26 +140,20 @@ delete()
 {
     case $1 in
             "nginx")
-                    kubectl delete -f srcs/kustomization/nginx.yaml > /dev/null
-                    echo "$red $bold Nginx.yaml deleted in Minikube ✗$neutre"
+                    kubectl delete -f srcs/kustomization/nginx.yaml &> /dev/null
+                    echo -e "$red ${bold}Nginx.yaml deleted in Minikube ✗$neutre"
                     ;;
             "wordpress")
-                    kubectl delete -n default deployment wordpress > /dev/null
-                    echo "$red $bold Wordpress deployment deleted in Minikube ✗ $neutre"
-                    kubectl delete -n default service wordpress > /dev/null
-                    echo "$red $bold Wordpress service deleted in Minikube ✗ $neutre"
+                    kubectl delete -f srcs/kustomization/wordpress.yaml &> /dev/null
+                    echo -e "$red ${bold}Wordpress.yaml deleted in Minikube ✗$neutre"
                     ;;
             "mysql")
-                    kubectl delete -n default deployment mysql > /dev/null
-                    echo "$red $bold Mysql deployment deleted in Minikube ✗ $neutre"
-                    kubectl delete -n default service mysql > /dev/null
-                    echo "$red $bold Mysql service deleted in Minikube ✗ $neutre"
+                    kubectl delete -f srcs/kustomization/mysql.yaml &> /dev/null
+                    echo -e "$red ${bold}Mysql.yaml deleted in Minikube ✗$neutre"
                     ;;
             "php")
-                    kubectl delete -n default deployment phpmyadmin > /dev/null
-                    echo "$red $bold Php deployment deleted in Minikube ✗ $neutre"
-                    kubectl delete -n default service phpmyadmin > /dev/null
-                    echo "$red $bold Php service deleted in Minikube ✗ $neutre"
+                    kubectl delete -f srcs/kustomization/phpmyadmin.yaml &> /dev/null
+                    echo -e "$red ${bold}Phpmyadmin.yaml deleted in Minikube ✗$neutre"
                     ;;
             *)
                     delete nginx
@@ -166,11 +176,11 @@ elif [ "$1" = "build" ]; then
 elif [ "$1" = "del" ]; then
     delete $2;
 elif [ "$1" = "apply" ]; then
-    apply $2;
+    apply;
 elif [ "$1" = "update" ]; then
     delete $2
     build $2 $3
-    apply $2;
+    apply;
 elif [ !$1 ]; then
-    /bin/echo "muk";
+    /bin/echo -e "muk";
 fi
